1e39a7270b76bc452f94aae9a9b27731cee1b684
Amanullah23 <amanyawari220@gmail.com>
2026-09-05 10:31:24 +0430

    some fixed on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "dzndefhoezgbsblxzamo.supabase.co" },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/blog/:path*",
+        destination: "/",
+        permanent: false,
+      },
+    ];
   },
 };
 
